82ed340c9c0f45149b7b5389d3e325d29d64c82b
Nitish Kumar Sharma <nitish.sharma@thoughtworks.com>
2020-02-07 18:51:53 +0530

    Req 1.3 | Refactor. changed books from a static to non static field in BibliotecaApp

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class BibliotecaApp {
     private static final String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-    private static final List<Book> books = Arrays.asList(new Book("Book1", "Author1", 1999), new Book("Book2", "Author2", 1990));
+    private final List<Book> books = Arrays.asList(new Book("Book1", "Author1", 1999), new Book("Book2", "Author2", 1990));
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
