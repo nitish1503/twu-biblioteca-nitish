e9d71e8fed5a14fd98d7bd48e83430b814e2ecf4
Nitish Kumar Sharma <nitish.sharma@thoughtworks.com>
2020-02-07 17:28:28 +0530

    Req 1.2 | Biblioteca can now show a list of all books after welcome message

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BibliotecaApp {
     public static final String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.welcome();
+        System.out.println(bibliotecaApp.welcome());
+        System.out.println(bibliotecaApp.showBookList());
     }
 
     public String welcome() {
         return welcomeMessage;
+    }
+
+    public ArrayList<String> showBookList() {
+        return new ArrayList<>(Arrays.asList("Book1", "Book2", "Book3"));
     }
 }
--- src/test/java/BibliotecaAppTest.java
+++ src/test/java/BibliotecaAppTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class BibliotecaAppTest {
 
     @Test
@@ -11,5 +14,15 @@ class BibliotecaAppTest {
         String actualMessage = bibliotecaApp.welcome();
 
         Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void shouldBeAbleToShowTheListOfBooks() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        ArrayList<String> expectedBookList = new ArrayList<>(Arrays.asList("Book1", "Book2", "Book3"));
+
+        ArrayList<String> actualBookList = bibliotecaApp.showBookList();
+
+        Assertions.assertEquals(expectedBookList, actualBookList);
     }
 }
