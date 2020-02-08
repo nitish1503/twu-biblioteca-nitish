7694118216d67bcc80eb2555d7ef35e76efe6f83
Nitish Kumar Sharma <nitish.sharma@thoughtworks.com>
2020-02-09 02:24:49 +0530

    Req 1.4 | Refactor. added menu field in BibliotecaApp and showMenu method displays it

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class BibliotecaApp {
     private static final String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    private static final String menu = "1. List of books";
     private final List<Book> books = Arrays.asList(new Book("Book1", "Author1", 1999), new Book("Book2", "Author2", 1990));
 
     public static void main(String[] args) {
@@ -21,6 +22,6 @@ public class BibliotecaApp {
     }
 
     public void showMenu() {
-        System.out.println("1. List of books");
+        System.out.println(menu);
     }
 }
