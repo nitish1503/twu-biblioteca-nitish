fbe260dae1608e1e7089e81cdf7c5bb6c9fef0f5
Nitish Kumar Sharma <nitish.sharma@thoughtworks.com>
2020-02-10 16:47:16 +0530

    Refactor. changed books from a field in BibliotecaApp to a local variable in the contructor

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class BibliotecaApp {
     private static final String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-    protected final List<Book> books = Arrays.asList(new Book("Book1", "Author1", 1999), new Book("Book2", "Author2", 1990));
 
     private static Menu menu;
 
     public BibliotecaApp() {
+        List<Book> books = Arrays.asList(new Book("Book1", "Author1", 1999), new Book("Book2", "Author2", 1990));
         BookShelf bookShelf = new BookShelf(books);
         menu = new Menu(bookShelf);
     }
